a513e0356c10f718352960c557dc9938740460b9
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-03-20 00:22:33 -0300

    add comando allClear

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -2,7 +2,19 @@ class Memory {
   String _value = '0';
 
   void applyCommand(String command){
+    if(command == 'AC'){
+
+    _allClear();
+    } else {
     _value += command;
+
+
+    }
+
+  }
+
+  _allClear() {
+    _value = '0';
   }
 
   String get value {
